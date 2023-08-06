a487c22a0c6fe91db10cf389117b9cd74b33c7ac
walter <waltermedinaberrios@gmail.com>
2023-08-06 14:48:59 -0500

    Cambio de base url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,26 +1,24 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
-    prepareHeaders: (headers, { getState }) => {
-        const token = getState().localStorage.token;
-        if(token){
-            headers.set("authorization", `Bearer ${token}`)
-        }
-        return headers;
+  baseUrl: "https://authorization-user-db.onrender.com/api/v1",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().localStorage.token;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
     }
-})
-
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
-    reducerPath: "posts",
-    baseQuery,
-    endpoints: build => ({
-        getLoggedUser: build.query({
-            query: () => '/users/me'
-        })
-    })
-})
+  reducerPath: "posts",
+  baseQuery,
+  endpoints: (build) => ({
+    getLoggedUser: build.query({
+      query: () => "/users/me",
+    }),
+  }),
+});
 
-export const { useGetLoggedUserQuery } = apiSlice
+export const { useGetLoggedUserQuery } = apiSlice;
